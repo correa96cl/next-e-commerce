4f93b04d72f032557f77bc41efac6a5693a14de6
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-02-08 21:20:08 -0300

    COmit

--- devstore/src/app/(store)/product/[slug]/page.tsx
+++ devstore/src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,51 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 
-export default function ProductPage() {
+interface ProductProps {
+  params: {
+    slug: string
+  }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60,
+    },
+  })
+
+  const product = await response.json()
+  console.log('product : ', product)
+
+  return product
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+  return {
+    title: product.title,
+  }
+}
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+  const product = await getProduct(params.slug)
   return (
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/moletom-never-stop-learning.png"
+          src={product.image}
           alt=""
           width={1000}
           quality={100}
@@ -13,18 +53,24 @@ export default function ProductPage() {
         />
       </div>
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          Moletom Never Stop Learning
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12 % de poliester.
+          {product.description}
         </p>
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-400 px-5 py-2.5 font-semibold">
-            R$129
+            {product.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de R$10,75
+            {(product.price / 12).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
           </span>
         </div>
         <div className="mt-8 space-y-4">
