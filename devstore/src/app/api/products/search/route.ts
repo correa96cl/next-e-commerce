bb68f56b8906e69bd0aa4597daa9eba619ecb906
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-02-18 15:12:58 -0300

    Procura

--- devstore/src/app/api/products/search/route.ts
+++ devstore/src/app/api/products/search/route.ts
@@ -2,10 +2,7 @@ import { z } from 'zod'
 import type { NextRequest } from 'next/server'
 import data from '../data.json'
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { slug: string } },
-) {
+export async function GET(request: NextRequest) {
   await new Promise((resolve) => setTimeout(resolve, 1000))
 
   const { searchParams } = request.nextUrl
